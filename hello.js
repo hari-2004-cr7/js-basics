d010bc38d48ea2cdc84bf5d6a582c4f8b25923cd
Hariharan.B <haribalaji8104@gmail.com>
2022-11-27 08:47:47 +0000

    Create 4 files

---
+++ hello.js
@@ -0,0 +1,41 @@
+//console.log("hello world");
+//console.log("1"+1);
+//console.log(123456789);
+//console.log(typeof "ayush");
+// const cannot be changed
+
+//const name="hari";
+//const another_name="karandeep"+"hari";
+// declare which can be change value
+//let name="Aush"
+//name="hari"
+//console.log(name);
+
+//const a=[1,"hari",null,undefined];
+//console.log(a[2]);
+//console.log(typeof a);
+
+//json similar dictionary
+//const a={
+    //name:"hari",
+   // age:18,
+    
+//}
+// FUNCTIONS
+
+//function square(x){
+  //  return x*x
+
+//const a=10;
+//const b=20;
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "jari is ppro guy",
+    e : "oO",
+    T : "U "
+}));
+ 
+
+
+
